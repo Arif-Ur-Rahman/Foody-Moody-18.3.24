f1269eb74d318007a3f7ece483248156c6a13eda
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-03-20 12:19:29 +0600

    Menu

--- src/components/Shef/index.jsx
+++ src/components/Shef/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import Service from './Service'
+import Footer from '../../container/Footer/Footer'
 
 function Shef() {
   return (
     <div>
     <Hero />
     <Service />
+    <Footer />
     </div>
   )
 }
