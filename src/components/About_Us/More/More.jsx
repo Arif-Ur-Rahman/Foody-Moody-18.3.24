c9653c3422e0dbb45cb1da87b12cc4f66a92d66f
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-03-31 12:18:02 +0600

    Updated

--- src/components/About_Us/More/More.jsx
+++ src/components/About_Us/More/More.jsx
@@ -1,48 +1,43 @@
-import React from 'react';
-import Image from '../../../assets/About/about-item-menu.png';
-import '../responsive.css';
-import './More.css'
+import React from "react";
+import Image from "../../../assets/About/about-item-menu.png";
+import "../responsive.css";
+import "./More.css";
+import { Link } from "react-router-dom";
 
 const More = () => {
-    return (
-        <section className="about">
-            <img src="../../../assets/About/about-item-menu.png" className="about-site-img" alt="" />
-            <div className="about-container">
-                <div className="about-img-container">
-                    <img src={Image} className="about-image" alt="" />
-                </div>
-                <div className="about-text-container">
-                    <div className="about-headings">
-                        
-                        <h1 className="about-main-heading">
-                            Food Is An Important Part Of A Balanced Diet
-                        </h1>
-                        <p className="about-sub-heading">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Debitis ea magni quod dicta tenetur!
-                            Ea fugiat rem magni quasi eius quisquam.
-                            Qui commodi non quaerat exercitationem 
-                            sed, assumenda iure quos?
-                        </p>
-                        <p className="about-sub-heading">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Debitis ea magni quod dicta tenetur!
-                            Ea fugiat rem magni quasi eius quisquam.
-                        </p>
-                        <p className="about-sub-heading">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Debitis ea magni quod dicta tenetur!
-                            Ea fugiat rem magni quasi eius quisquam.
-                        </p>
-                        <div className="about-btns">
-                            <a href="#" className="action-btn">Learn More</a>
-                            <a href="#" className="action-btn-two"><i className="fa fa-video-camera"></i> Watch Video</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section className="about">
+      <img
+        src="../../../assets/About/about-item-menu.png"
+        className="about-site-img"
+        alt=""
+      />
+      <div className="about-container">
+        <div className="about-img-container">
+          <img src={Image} className="about-image" alt="" />
+        </div>
+        <div className="about-text-container">
+          <div className="about-headings">
+            <h1 className="about-main-heading">
+              Tasty Food Is Challenge And Foody Moody Assures That
+            </h1>
+            <p className="about-sub-heading">
+              Indulge in Culinary Adventures with Foody Moody: Where Tasty Food
+              is a Triumph. Embark on a Flavorful Journey with Foody Moody:
+              Where Every Bite is a Delicious Challenge Met!
+            </p>
+            
+            
+            <div className="about-btns">
+              <Link to="/shef" className="action-btn">
+                Learn More
+              </Link>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default More;
