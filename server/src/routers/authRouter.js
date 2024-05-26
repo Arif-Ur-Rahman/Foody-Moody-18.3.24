9bf2cc005ad34fe2de70ad1d33c9869fe912fbeb
Sakib <sahadothosain6619@gmail.com>
2024-05-26 14:49:36 +0600

    added forgot password

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   LoginController,
+  forgotPasswordController,
   registerController,
   testController,
 } from "../controllers/authController.js";
@@ -10,7 +11,8 @@ const router = express.Router();
 router.post("/register", registerController);
 
 router.post("/login", LoginController);
-
+// forgot password
+router.post("/forgot-password", forgotPasswordController);
 // test routes
 router.get("/test", requireSignIn, isAdmin, testController);
 export default router;
