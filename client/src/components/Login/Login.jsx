2c0fd11045404ad9a41b5c77dc72860b41d17e18
Dream-Diver-Co <dreamdiver2017@gmail.com>
2024-05-26 13:10:26 +0600

    Updated

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -19,23 +19,21 @@ const Login = () => {
       <form onSubmit={handleSubmit} className="login-form">
         <h2>Login</h2>
         <div className="form-group">
-          <label htmlFor="email">Email:</label>
           <input
             type="email"
             id="email"
+            placeholder="Email" required=""
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            required
           />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password:</label>
           <input
             type="password"
+            placeholder="Password" required=""
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            required
           />
         </div>
         <button type="submit" className="login-btn">Login</button>
